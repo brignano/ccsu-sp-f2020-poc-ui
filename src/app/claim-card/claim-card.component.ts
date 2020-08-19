06ad35ebd739b35d9dc33250a9c66f9ad2558f99
Anthony Brignano <abrignano@comcast.net>
2020-08-19 11:19:39 -0400

    setup basic structure

--- src/app/claim-card/claim-card.component.ts
+++ src/app/claim-card/claim-card.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Claim } from './claim.model';
 
 @Component({
   selector: 'app-claim-card',
   templateUrl: './claim-card.component.html',
   styleUrls: ['./claim-card.component.scss']
 })
+
 export class ClaimCardComponent implements OnInit {
+  @Input() claim: Claim;
 
   constructor() { }
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Claim } from '../claim-card/claim.model';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  @Input() claims: Claim[];
 
   constructor() { }
 

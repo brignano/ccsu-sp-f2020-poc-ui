ee632a00598af31eca1148b1001800d923a6469f
Anthony Brignano <abrignano@comcast.net>
2020-08-22 10:49:05 -0400

    add build:prod script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  claims_api: 'http://localhost:4200/api/'
+  claims_api: 'http://localhost:4200/claims/',
+  geocode_api: 'http://localhost:4200/geocode/'
 };
